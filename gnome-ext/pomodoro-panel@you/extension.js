c3b107f0d77dfac20a562f530e278fd97c0b3b54
qchi2023 <126528024+qchi2023@users.noreply.github.com>
2025-10-22 17:03:10 -0500

    Refactor WebSocket connection and update handling

--- gnome-ext/pomodoro-panel@you/extension.js
+++ gnome-ext/pomodoro-panel@you/extension.js
@@ -1,6 +1,7 @@
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
+import Clutter from 'gi://Clutter';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import Soup from 'gi://Soup';
@@ -15,22 +16,39 @@ export function enable() {
     log('[PomodoroPanel] Enabling WebSocket extension...');
     try {
         button = new PanelMenu.Button(0.0, 'PomodoroPanel', false);
+
         label = new St.Label({
             text: 'Pomodoro: Connecting...',
             y_align: Clutter.ActorAlign.CENTER,
         });
+
         button.add_child(label);
         Main.panel.addToStatusArea('pomodoro-panel', button);
 
+<<<<<<< HEAD
         wsSession = new Soup.Session();
         connectWebSocket();
     } catch (e) {
         logError(e, '[PomodoroPanel] enable() failed');
+=======
+        session = new Soup.Session();
+
+        updatePomodoro();
+        updateLoopId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
+            updatePomodoro();
+            return GLib.SOURCE_CONTINUE;
+        });
+
+        log('[PomodoroPanel] Extension enabled.');
+    } catch (e) {
+        logError(e, '[PomodoroPanel] enable() failed.');
+>>>>>>> 5f14d65 (WIP: Save local changes before pulling latest)
     }
 }
 
 function connectWebSocket() {
     try {
+<<<<<<< HEAD
         log('[PomodoroPanel] Connecting to relay...');
         Soup.Session.prototype.websocket_connect_async.call(
             wsSession,
@@ -66,6 +84,18 @@ function connectWebSocket() {
                     label.set_text('Pomodoro: Error');
                     reconnectLater();
                 }
+=======
+        const msg = Soup.Message.new('GET', 'http://localhost:4545/status');
+        session.send_and_read_async(msg, 0, null, (src, res) => {
+            try {
+                const bytes = session.send_and_read_finish(res);
+                const data = JSON.parse(new TextDecoder().decode(bytes.get_data()));
+                const displayText = `${data.task || 'No Task'} (${data.time || '--:--'})`;
+                label.set_text(displayText);
+            } catch (err) {
+                logError(err, '[PomodoroPanel] Failed to parse response');
+                label.set_text('Pomodoro: Error');
+>>>>>>> 5f14d65 (WIP: Save local changes before pulling latest)
             }
         );
     } catch (err) {
@@ -97,7 +127,12 @@ function handleMessage(text) {
             }
         }
     } catch (e) {
+<<<<<<< HEAD
         logError(e, '[PomodoroPanel] Failed to parse WS message');
+=======
+        logError(e, '[PomodoroPanel] updatePomodoro() failed');
+        label.set_text('Pomodoro: Error');
+>>>>>>> 5f14d65 (WIP: Save local changes before pulling latest)
     }
 }
 
@@ -120,9 +155,16 @@ export function disable() {
             wsSession.abort();
             wsSession = null;
         }
+<<<<<<< HEAD
         label = null;
         log('[PomodoroPanel] Extension disabled cleanly.');
     } catch (e) {
         logError(e, '[PomodoroPanel] disable() failed');
+=======
+
+        log('[PomodoroPanel] Extension disabled.');
+    } catch (e) {
+        logError(e, '[PomodoroPanel] disable() failed.');
+>>>>>>> 5f14d65 (WIP: Save local changes before pulling latest)
     }
 }
